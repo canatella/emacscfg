f4f3325bd60e7ffa0df1ded67d6583130911521c
Damien Merenne <dam@cosinux.org>
2012-02-06 09:56:04 +0100

    fix completion

--- softathome.el
+++ softathome.el
@@ -20,9 +20,9 @@
   (ido-switch-buffer)
   (sah-configure-p4))
 
-;;; semantic
+;;; c-mode-hook
 (require 'semantic/analyze/refs)
-(defun my-sah-setup-semantic ()
+(defun my-sah-c-mode-hook ()
   (interactive)
   (if sah-project
       (let ((sysinc (format "%sinclude" (or (sah-project-component-directory "sah_toolchain") "/usr/")))
@@ -37,16 +37,56 @@
         (semantic-reset-system-include 'c-mode)
         (semantic-add-system-include sysinc 'c-mode)
         (semantic-add-system-include staginginc 'c-mode)
-        ;;; autocomplete
-        ;;(add-to-list 'ac-sources 'ac-source-semantic-raw)
+        ;;; completion
+        (local-set-key (kbd "M-TAB") 'my-semantic-complete)
+        ;;; swith to header
+        (local-set-key (kbd "A-o") 'ff-find-other-file)
         ;;; flymake
         (message "setting up flymake")
         (setenv "INCLUDE" sysinc)
         (flymake-mode 't)
-        (setq flymake-get-project-include-dirs-function (lambda () staginginc))
+        (setq flymake-get-project-include-dirs-function '(basedir staginginc))
         (define-key c-mode-map "\C-c\C-v" 'flymake-display-err-menu-for-current-line)
         (define-key c-mode-map (kbd "A-j") 'semantic-ia-fast-jump)
         (subword-mode 't))))
+
+(defun my-semantic-format-complete (tag)
+  (semantic-format-tag-abbreviate tag nil t))
+
+(defun my-semantic-completions () 
+  (let* ((start (car (nth 2 (semantic-ctxt-current-symbol-and-bounds (point)))))
+         (pattern (regexp-quote (buffer-substring start (point))))
+         (syms (semantic-analyze-possible-completions (semantic-analyze-current-context (point))))
+         collection)
+         (if (null syms)
+             (if (and semantic--completion-cache
+                      (eq (nth 0 semantic--completion-cache) (current-buffer))
+                      (=  (nth 1 semantic--completion-cache) start)
+                      (save-excursion
+                        (goto-char start)
+                        (looking-at (nth 3 semantic--completion-cache))))
+                 ;; Use cached value.
+                 (setq collection (nthcdr 4 semantic--completion-cache))
+               ;; Perform new query.
+               (setq collection (semantic-find-tag-for-completion pattern))
+               (setq semantic--completion-cache
+                     (append (list (current-buffer) start 0 pattern)
+                             collection)))
+           syms)))
+
+(setq my-semantic-completion-tag-format 'my-semantic-format-complete)
+
+(defun my-semantic-complete ()
+  (interactive)
+  (let* ((start (car (nth 2 (semantic-ctxt-current-symbol-and-bounds (point)))))
+         (syms (my-semantic-completions))
+         (sym (if (eql 1 (length syms)) (car syms)
+                (let ((completion (ido-completing-read "Complete" 
+                                                       (mapcar my-semantic-completion-tag-format syms))))
+                  (car (remove* completion syms :test-not 'string= :key my-semantic-completion-tag-format))))))
+    (delete-region start (point))
+    (insert (semantic-format-tag-name sym))))
+         
 
 (defadvice flymake-start-syntax-check-process (after
                                                cheeso-advice-flymake-start-syntax-check-1
@@ -83,5 +123,5 @@
 (global-set-key (kbd "C-c C-M-o") 'sah-open-any-project)
 
 ;;; hooks
-(add-hook 'c-mode-common-hook 'my-sah-setup-semantic)
+(add-hook 'c-mode-common-hook 'my-sah-c-mode-hook)
 
