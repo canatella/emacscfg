d783a0f23cff5bf4219560eea2248c5401991dcc
Damien Merenne <dn@dsa-ac.de>
2013-05-08 14:44:11 +0200

    - add function to copy file when a buffer is saved

--- editing.el
+++ editing.el
@@ -20,3 +20,19 @@
   (when buffer-file-name
         (setq buffer-read-only (not buffer-read-only))))
 
+;;; save a copy of the file
+(defun my-file-save-copy-gen (path)
+  (lambda ()
+    (let ((orig (buffer-file-name)))
+      (write-file (concat path (file-name-nondirectory orig)) nil)
+      (write-file orig nil))))
+
+(defun my-file-save-copy (path)
+  (interactive "FCopy path:")
+  (add-hook 'local-write-file-hooks
+            (lexical-let ((p path))
+              (lambda ()
+                (let ((orig (buffer-file-name)))
+                  (write-file p nil)
+                  (write-file orig nil))))))
+
