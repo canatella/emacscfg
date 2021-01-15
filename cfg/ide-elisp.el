07aac8a30042251b47c7b9bae7f881aa41921943
Damien Merenne <damien@bloom-life.com>
2021-01-15 06:54:36 +0100

    Fix elisp format.

--- cfg/ide-elisp.el
+++ cfg/ide-elisp.el
@@ -10,14 +10,13 @@
 
 (use-package  elisp-mode :dash (emacs-lisp-mode "Emacs_Lisp"))
 
-(eval-after-load 'elisp-format
-  (progn
-    (define-minor-mode elisp-format-on-save-mode
-      "When enabled, call `elisp-format-buffer' when this buffer is saved."
-      nil
-      :global nil
-      (if elisp-format-on-save-mode
-          (add-hook 'before-save-hook #'elfmt nil t)
-        (remove-hook 'before-save-hook #'elfmt t)))))
+(use-package elisp-format :ensure t
+  :config (define-minor-mode elisp-format-on-save-mode
+            "When enabled, call `elisp-format-buffer' when this buffer is saved."
+            nil
+            :global nil
+            (if elisp-format-on-save-mode
+                (add-hook 'before-save-hook #'elfmt nil t)
+              (remove-hook 'before-save-hook #'elfmt t))))
 
 (use-package form-feed :ensure t :diminish :hook (emacs-lisp-mode . form-feed-mode))
