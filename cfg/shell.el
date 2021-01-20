ee837ba651356ac18884a9980081197b15f94901
Damien Merenne <damien@bloom-life.com>
2021-01-20 18:33:39 +0100

    Add custom commands to vterm.

--- cfg/shell.el
+++ cfg/shell.el
@@ -1,3 +1,8 @@
 (use-package vterm :ensure t :custom
   (vterm-kill-buffer-on-exit t)
+  (vterm-eval-cmds
+   '(("find-file" find-file)
+     ("message" message)
+     ("vterm-clear-scrollback" vterm-clear-scrollback)
+     ("woman" woman)))
   :bind (("C-c e" . vterm-end-of-line)))
