c7fd55f93465e251ff8f1f5bd4a73a57a7f2db2c
Damien Merenne <damien@bloom-life.com>
2022-09-28 12:15:07 +0200

    Load structurizr mode for .c4 files.

--- cfg/ide-asciidoc.el
+++ cfg/ide-asciidoc.el
@@ -1,8 +1,9 @@
 (use-package trivial-adoc-mode :quelpa
   (trivial-adoc-mode :fetcher github :repo "canatella/emacs-trivial-adoc-mode"))
 
-(use-package structurizr-mode :quelpa
-  (structurizr-mode :fetcher github :repo "canatella/structurizr-mode" :branch "fix-package-header"))
+(use-package structurizr-mode
+  :mode "\\.c4\\'"
+  :quelpa (structurizr-mode :fetcher github :repo "canatella/structurizr-mode" :branch "fix-package-header"))
 
 (use-package plantuml-mode
   :ensure t
