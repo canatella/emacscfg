e0b95b00825a97b93a11763dd9166c9dafa55b7e
Damien Merenne <damien@bloom-life.com>
2023-02-06 10:11:07 +0100

    Add restclient configuration

---
+++ cfg/ide-http.el
@@ -0,0 +1,45 @@
+;; -*- lexical-binding: t; -*-
+
+(use-package restclient :ensure t
+  :after mmm-mode
+   :mode ("\\.http\\'" . restclient-mode)
+  :config
+  (mmm-add-group 'restclient-elisp
+                 '((restclient-elisp-variable-multiline :submode emacs-lisp-mode
+                                              :face mmm-code-submode-face
+                                              :delimiter-mode nil
+                                              :front ":= <<\n" :back "\n#\n")
+                   (restclient-elisp-variable-singleline :submode emacs-lisp-mode
+                                              :face mmm-code-submode-face
+                                              :delimiter-mode nil
+                                              :front ":= (" :back ")\n")
+                   (restclient-json-body :submode js-mode
+                                              :face mmm-code-submode-face
+                                              :delimiter-mode nil
+                                              :front "\n\n{" :back "}\n\n")))
+  (add-to-list 'mmm-mode-ext-classes-alist
+                   '(restclient-mode "\\.http\\'" restclient-elisp)))
+
+(use-package restclient-jq :ensure t)
+
+(cl-defun biot-build-search-request (&key filter)
+  (url-hexify-string (json-serialize `(filter: ,filter))))
+
+(cl-defun biot-build-search-filter (parameter &key in eq lt lte gt gte like from to is-null is-not-null not)
+  (let ((filters (seq-filter #'identity (list (when in `(in . ,in))
+                                              (when eq `(eq . ,eq))
+                                              (when lt `(lt . ,lt))
+                                              (when lte `(lte . ,lte))
+                                              (when gt `(gt . ,gt))
+                                              (when gte `(gte . ,gte))
+                                              (when like `(like . ,like))
+                                              (when from `(from . ,from))
+                                              (when to `(to . ,to))
+                                              (when is-null `(isNull . ,is-null))
+                                              (when is-not-null `(isNotNull . ,is-not-null))
+                                              (when not `(not . ,not))))))
+    `(,parameter . (,filters))))
+
+(cl-defmacro biot-search-request (&key filter)
+  "Build a BioT search request query argument using FILTER."
+  `(biot-build-search-request :filter (biot-build-search-filter (quote ,(car filter)) ,@(cdr filter))))
